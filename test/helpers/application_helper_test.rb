1d98b6d90210c0eb3bab9051bbbf156aa9045994
KennethAshworth <Kennethjashworth@gmail.com>
2016-08-27 17:03:54 -0500

    added integration tests

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+	test "full tutle helper" do
+		assert_equal full_title, "Ruby on Rails Tutorial"
+		assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial"
+	end
