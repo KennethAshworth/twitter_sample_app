0792248efa6bb5882e6fb8d3783b1404425e6b36
KennethAshworth <Kennethjashworth@gmail.com>
2016-08-27 17:09:40 -0500

    fix class ApplicationHelperTest

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-	test "full tutle helper" do
-		assert_equal full_title, "Ruby on Rails Tutorial"
-		assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial"
+	test "full title helper" do
+		assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+		assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
 	end
+end
